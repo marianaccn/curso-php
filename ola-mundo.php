5644ef92730064e5b9fd9d5bf5c52a1429fad25a
Mariana Cavalcante <marianacavalcante.nasc@gmail.com>
2021-07-07 13:45:32 -0300

    Exercício php

---
+++ ola-mundo.php
@@ -0,0 +1,17 @@
+<?php 
+
+$idade = 16;
+$numeroDePessoas = 3;
+
+echo "Entrada permitida a partir de 18 anos, ou ";
+echo "a partir de 16 anos acompanhado" . PHP_EOL;
+
+if ($idade >= 18){
+    echo "Você tem $idade anos, pode entrar sozinho" . PHP_EOL; 
+}else if ($idade >= 16 && $numeroDePessoas > 1){
+    echo "Você tem $idade anos, está acompanhado(a), pode entrar";
+} else {
+    echo "Você só tem $idade anos. Você não pode entrar.";
+}
+
+ 
